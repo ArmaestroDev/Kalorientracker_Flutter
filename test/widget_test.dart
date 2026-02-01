075698a8fa1d0f9c01edc897e1c97a91233a9aa0
ArmaestroDev <marmando@gmx.de>
2026-02-01 14:32:54 +0100

    icon change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kalorientracker_flutter/main.dart';
 
 void main() {
   testWidgets('App loads successfully', (WidgetTester tester) async {
